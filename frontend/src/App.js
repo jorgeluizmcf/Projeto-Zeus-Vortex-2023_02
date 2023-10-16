378d24d74a64c1536ad388040a485a71f0e16b7a
jorgeluizmcf <jorgeluizmcf@gmail.com>
2023-10-16 17:36:29 -0300

    implementando botões, categories-boxes e seleção de período

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,136 +1,163 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './styles/App.css';
 import './styles/global.css';
 import './styles/sidebar.css';
 import './styles/category-box.css';
+import './styles/dashboard.css';
 import fotoPet from './img/foto-pet-default.jpg';
+import iconeRacao from './img/icone-ração.png';
+import iconeHigiene from './img/icone-higiene.png';
+import iconeBrinquedos from './img/icone-brinquedos.png';
+import iconeVeterinario from './img/icone-veterinario.png';
 
 function App() {
+  //importando toggleFlag
+  const [isFlagged, setFlag] = useState(true);
+  const toggleFlag = () => { setFlag(!isFlagged); };
+
   return (
     
-    // Implementação default do React
-
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-
     <div id="app">
 
-      <>
-        <aside className="sidebar">
-          <img className="foto-pet" src={ fotoPet } alt="Foto pet" />
-          <strong className="nome-pet">Zeus</strong>
+      <aside className="sidebar">
+        <img className="foto-pet" src={ fotoPet } alt="Foto pet" />
+        <strong className="nome-pet">Zeus</strong>
 
-          <div className="button-group">
-            <button className="sidebar-button">Botão 1</button>
-            <button className="sidebar-button">Botão 2</button>
-            <button className="sidebar-button">Botão 3</button>
-          </div>
+        <div className="button-group">
+          <button className="sidebar-button">Adicionar</button>
+          <button className="sidebar-button">Alterar</button>
+          <button className="sidebar-button">Apagar</button>
+        </div>
 
-          <div className="separator"></div>
+        <div className="separator"></div>
 
-          <button className="logoff-button">Logoff</button>
-        </aside>
-      </>
-
-      <aside className="category-box-container">
-        <>
-          <aside className="category-box">
-            <strong>Alimentação</strong>
-            <form>
-
-              <div className="input-block">
-                <label htmlFor="title">Alimentação</label>
-                <input />
-              </div>
-
-              <div className="input block">
-                <label htmlFor="nota">Valor</label>
-                <textarea></textarea>
-              </div>
-
-              <button type="submit">Salvar</button>
-            </form>
-          </aside>
-        </>
-
-        <>
-          <aside className="category-box">
-            <strong>Higiene</strong>
-            <form>
-
-              <div className="input-block">
-                <label htmlFor="title">Higiene</label>
-                <input />
-              </div>
-
-              <div className="input block">
-                <label htmlFor="nota">Valor</label>
-                <textarea></textarea>
-              </div>
-
-              <button type="submit">Salvar</button>
-            </form>
-          </aside>
-        </>
-
-        <>
-          <aside className="category-box">
-            <strong>Brinquedos e Acessórios</strong>
-            <form>
-
-              <div className="input-block">
-                <label htmlFor="title">Brinquedos e Acessórios</label>
-                <input />
-              </div>
-
-              <div className="input block">
-                <label htmlFor="nota">Valor</label>
-                <textarea></textarea>
-              </div>
-
-              <button type="submit">Salvar</button>
-            </form>
-          </aside>
-        </>
-
-        <>
-          <aside className="category-box">
-            <strong>Veterinário</strong>
-            <form>
-
-              <div className="input-block">
-                <label htmlFor="title">Veterinário</label>
-                <input />
-              </div>
-
-              <div className="input block">
-                <label htmlFor="nota">Valor</label>
-                <textarea></textarea>
-              </div>
-
-              <button type="submit">Salvar</button>
-            </form>
-          </aside>
-        </>
+        <button className="logoff-button">Logoff</button>
       </aside>
 
+      <div className='screen-container'> 
+        <h1>Categorias</h1>
+        
+        <div className='period-selecter'>
+          <h2>Período</h2>
+          
+          <form>
+            <label for="periodo-mes">Mês:</label>
+            <select id="periodo-mes" name="periodo-mes">
+              <option value="janeiro">janeiro</option>
+              <option value="fevereiro">fevereiro</option>
+              <option value="março">março</option>
+              <option value="abril">abril</option>
+              <option value="maio">maio</option>
+              <option value="junho">junho</option>
+              <option value="julho">julho</option>
+              <option value="agosto">agosto</option>
+              <option value="setembro">setembro</option>
+              <option value="outubro">outubro</option>
+              <option value="novembro">novembro</option>
+              <option value="dezembro">dezembro</option>
+            </select>
+          </form>
+
+          <form>
+            <label for="periodo-ano">Ano:</label>
+            <select id="periodo-ano" name="periodo-ano">
+              <option value="2023">2023</option>
+              <option value="2022">2022</option>
+            </select>
+          </form>
+        </div>
+
+
+        <div className="category-box-container">
+          <div className="category-box">
+              <div className='flag-button-container'>
+
+                <button className="flag-button" onClick={() => toggleFlag()}>
+                    <i className="fas fa-flag"></i>
+                  </button>
+                </div>
+                
+                <strong className='category-title'>
+                  Alimentação
+                  <img className="category-icon" src={ iconeRacao } alt="icone de ração" />                
+                </strong>
+
+                <div className="price-info">
+                  <span>R$</span>
+                  <span className="calculated-value">1000,00</span>
+                </div>
+              </div>
+
+              <div className="category-box">
+                <div className='flag-button-container'>
+
+                <button className="flag-button" onClick={() => toggleFlag()}>
+                    <i className="fas fa-flag"></i>
+                  </button>
+                </div>
+                
+                <strong className='category-title'>
+                  Higiene
+                  <img className="category-icon" src={ iconeHigiene } alt="icone de Higiene" />                
+                </strong>
+
+                <div className="price-info">
+                  <span>R$</span>
+                  <span className="calculated-value">200,00</span>
+                </div>
+              </div>
+
+              <div className="category-box">
+                <div className='flag-button-container'>
+
+                <button className="flag-button" onClick={() => toggleFlag()}>
+                    <i className="fas fa-flag"></i>
+                  </button>
+                </div>
+                
+                <strong className='category-title'>
+                  Brinquedos
+                  <img className="category-icon" src={ iconeBrinquedos } alt="icone de brinquedos" />                
+                </strong>
+
+                <div className="price-info">
+                  <span>R$</span>
+                  <span className="calculated-value">200,00</span>
+                </div>
+              </div>
+
+              <div className="category-box">
+                <div className='flag-button-container'>
+
+                <button className="flag-button" onClick={() => toggleFlag()}>
+                    <i className="fas fa-flag"></i>
+                  </button>
+                </div>
+                
+                <strong className='category-title'>
+                  Veterinário
+                  <img className="category-icon" src={ iconeVeterinario } alt="icone de veterinário" />                
+                </strong>
+
+                <div className="price-info">
+                  <span>R$</span>
+                  <span className="calculated-value">200,00</span>
+                </div>
+          </div>
+        </div>
+
+        <div className='dashboard'>
+
+          <h3>Dashboard</h3>
+
+          <div className='dashboard-container'>
+            conteudo
+          </div>
+        </div>
+
+      </div>
     </div>
-
-
   );
 }
 
